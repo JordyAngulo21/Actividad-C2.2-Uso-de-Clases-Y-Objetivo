dc98c1c16afca18569f92fb43ea8e33dab426197
Edis Jipson Sosa Quiñonez <86238971+Edis-Jipson-Sosa-Quinonez@users.noreply.github.com>
2021-07-21 02:14:54 -0500

    Índice de masa corporal
    Realice el cálculo del índice de la masa corporal de una persona, para eso la persona debe ingresar su peso y su altura, también utilice las estructuras de condición "IF" que dependiendo del índice de la masa corporal que salga como resultado, se le enviara un mensaje a la persona de lo bien o mal que esta de salud sobre su cuerpo, y se le presentara recomendaciones ya sea para bajar o subir de peso y que se encuentre saludable.

--- miclase.h
+++ miclase.h
@@ -42,4 +42,52 @@ class Persona{
 		
 		}
 
-
+	
+	        //**************************//
+                // EDIS JIPSON SOSA QUINONEZ//
+	        //**************************//
+	
+                void set_masa(int peso){
+    
+                p=0;
+	        altura=0;
+	
+                cout<<"digite su peso:   ";cin>>p;
+                cout<<"digite su estatura:   ";cin>>altura;
+	        masa= p/(altura*altura);
+	        cout<<"\n\nTu masa corporal es de:  "<<masa<<"\n\n";
+	
+	        if(masa<18.5){
+		cout<<"1. Necesitas comer balanceadamente: Consulta una dieta para consumir alimentos que te ayuden\n\n";
+		cout<<"2. Comienza con rutina ejercicios y luego aumenta la intensidad para ganar musculatura";
+		cout<<"\n\n3. Ayúdate con vitaminas y complemento para obtener músculos más rápido\n\n";
+	        }
+	        else{
+		if (masa>=18.5 && masa<=24.9){
+			cout<<"Felicidades!!, has conseguido obtener los objetivos prometidos :)\n";
+			cout<<"\ Sigue dietas y ejercicios para mantener tu masa corporal:\n\n";
+			cout<<"1. Come sano para que tu metabolismo se mantenga sano\n";
+			cout<<"2. Comer frutas entre comidas favorecerá a mantener tu masa corporal\n";
+			cout<<"3. Realiza rutinas de ejercicio con constantes para mantener tu físico\n\n",
+			cout<<"                                         sigue así ;)                                     \n";
+		}
+		else{
+			if (masa>=18.5 && masa<=24.9){
+			cout<<"Atención!! Tienes que disminuir tu peso corporal :(\n";
+			cout<<"\n1. Reduce el consumo de alimentos que sean ricos en azúcares y grasas\n";
+			cout<<"2. Realizar actividad física frecuente para mantener tu cuerpo activo (adultos)\n";
+			cout<<"3. Comer varias veces al día ayuda a mantener una buena salud y no sufrir consecuencias\n\n",
+			cout<<"                                   Combate el sobrepeso!!                                       \n";	
+			}
+			else{
+			cout<<"Estas en peligroooo!! :(\n";
+			cout<<"\n1. Sigue una dieta exclusivamente de alimentos saludables\n";
+			cout<<"2. Realizar una rutina de entrenamiento que aumenten su intensidad progresivamente.\n";
+			cout<<"3. Realiza caminatas diarias de 1/2 kilometro\n",
+			cout<<"4. Comer varias veces al día fruta, así cereales integrales y frutos secos\n\n",
+			cout<<"                                   Combate el sobrepeso!!                                       \n";	
+			}
+				
+			}
+		}
+		}
